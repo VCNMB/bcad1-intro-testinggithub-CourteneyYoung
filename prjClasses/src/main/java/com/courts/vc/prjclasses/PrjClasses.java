669fbf0ad9067545d443c69945142491b3943679
Courteney Young <coyoung@varsitycollege.co.za>
2025-03-04 11:27:48 +0200

    Added project files
    Added the netbeans files

---
+++ prjClasses/src/main/java/com/courts/vc/prjclasses/PrjClasses.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.courts.vc.prjclasses;
+
+/**
+ *
+ * @author young
+ */
+public class PrjClasses {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
